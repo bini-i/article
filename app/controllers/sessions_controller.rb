e57419bd2750d478a21fa901b56a0ed7db7b4176
bini-i <binyam.sh1@gmail.com>
2021-01-19 23:55:42 +0300

    Add authentication with has_secure_password method
    - add password_digest field to user table
    - whitelist password and password confirmation
    - session controller creates session by authentication password against
    digest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,19 @@
 class SessionsController < ApplicationController
 
     def new
-        
+        @user = User.new
     end
 
     def create
-        
+        user = User.find_by(name: params[:name])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect :root
+        else
+            render 'new'
+        end
     end
 
     def destroy
-
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,18 @@ class UsersController < ApplicationController
     end
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Account successfully created."
+            redirect_to :root
+        else
+            flash[:alert] = "Couldn't successfully create your account. Try again."
+            render 'new'
+        end
     end
 
+    private
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
 end
