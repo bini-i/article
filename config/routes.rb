b4739dd985290a5bc7efa6e6fd4597e16ce9ff9d
bini-i <binyam.sh1@gmail.com>
2021-01-19 14:00:54 +0300

    Update sessions/new and users/new views
    - added sign in and sign up forms using form_with

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[new create]
+  get '/sign_up', to: 'users#new'
+  get '/sign_in', to: 'sessions#new'
+  post '/sign_in', to: 'sessions#create'
+  resources :users, only: %i[create]
 end
