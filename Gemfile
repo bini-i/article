e9be3df12ae03ebb642f2b2a2b3bfb33b42e8090
bini-i <binyam.sh1@gmail.com>
2021-01-21 13:34:22 +0300

    Add bootstrap and styled the nav bar
    - bootstrap added using yarn and set to be complied with webpack

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 
   gem 'pry-rails'
+  gem "better_errors"
+  gem "binding_of_caller"
 end
 
 group :test do
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
+
+environment.plugins.append("provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment
+
