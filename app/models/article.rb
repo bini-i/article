b849df38b84ca9bb5e202c06edc3d749af6541b9
bini-i <binyam.sh1@gmail.com>
2021-01-21 16:35:24 +0300

    Add many-to-many association b/n users and articles
    - users has many votted_articles through votes association
    - articles has many voters through votes association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
     belongs_to :author, class_name: "User"
+
+    has_many :votes
+    has_many :voters, through: :votes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
     validates :name, presence: true
 
     has_many :articles, foreign_key: :author_id
+
+    has_many :votes
+    has_many :voted_articles, through: :votes, source: :article
 end
