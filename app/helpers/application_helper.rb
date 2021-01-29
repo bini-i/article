0fce4404ac9c1e3b3c56ee63aa2c49ae68aa6748
bini-i <binyam.sh1@gmail.com>
2021-01-29 11:18:41 +0300

    Refactor _article.html.erb, and _header.html.erb
    - take all the logic to display vote button  application_helper.rb ad display_vote_button
    method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,14 @@ module ApplicationHelper
   def get_vote(article)
     current_user.votes.where(article_id: article.id).take
   end
+
+  def display_vote_button(article)
+    if user_signed_in?
+      if current_user.already_voted?(article)
+        render "articles/vote", article: article, vote: "down", method: :delete, path: :article_vote_path
+      else
+        render "articles/vote", article: article, vote: "up", method: :post, path: :article_votes_path
+      end
+    end
+  end
 end
